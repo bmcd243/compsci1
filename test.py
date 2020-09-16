4d0cf4c59ba40eb6ddd30abe52a56398ef921267
bmcd243 <64990696+bmcd243@users.noreply.github.com>
2020-09-16 09:40:50 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -1,3 +1,26 @@
+
+
+answers = [1, 1, 2, 3, 1, 1, 1, 1, 1]
+
+for x in answers:
+    print(answers)
+
+
+
+
+
+
+for x in answers:
+
+    user = input("What is the correct order? Input 1, 2 or 3 as your answer ")
+    if user == answers[i]:
+        print("Correct")
+    else:
+        print("Incorrect")
+
+
+
+
 class Questions():
     def __init__(self, question, correct_answer):
         self.question = question
@@ -12,32 +35,3 @@ question_6 = Questions("The new code will establish for the first time a clear p
 question_7 = Questions("The new code will establish for the first time a clear pecking order in our streets, with different groups of road users taking different places in a hierarchy. Which shows the correct order - from highest priority to lowest?", "3")
 question_8 = Questions("The new code will establish for the first time a clear pecking order in our streets, with different groups of road users taking different places in a hierarchy. Which shows the correct order - from highest priority to lowest?", "3")
 question_9 = Questions("The new code will establish for the first time a clear pecking order in our streets, with different groups of road users taking different places in a hierarchy. Which shows the correct order - from highest priority to lowest?", "3")
-
-answers = {
-
-    "answer_1": "A",
-    "answer_2": "3",
-    "answer_3": "3",
-    "answer_4": "3",
-    "answer_5": "3",
-    "answer_6": "3",
-    "answer_7": "3",
-    "answer_8": "3",
-    "answer_9": "3"
-
-}
-
-print(answers)
-
-
-
-
-
-for i in range(9):
-
-    user = input("What is the correct order? Input 1, 2 or 3 as your answer ")
-    if user == answer_[i]:
-        print("Correct")
-    else:
-        print("Incorrect")
-
